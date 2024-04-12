e6689e7f269db811ebdc14724a9624410d24e5af
muhumbania <franklinmuhumbania@gmail.com>
2024-04-12 20:09:37 +0300

    Finished the logic for brightness adjustment

---
+++ index.js
@@ -0,0 +1,8 @@
+const bulbOn = document.querySelector('.bulbOn');
+const range = document.querySelector('.range');
+
+range.addEventListener('change', function(){
+    let value = Number(this.value);
+    bulbOn.style.opacity = value/100;
+});
+
