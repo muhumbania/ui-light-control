55f4121f6de49fff0f6497897db4fb52b7db5e9d
muhumbania <franklinmuhumbania@gmail.com>
2024-04-14 23:19:08 +0300

    Added the page title

--- ESP8266_Webserver_Station_Mode/ESP8266_Webserver_Station_Mode.ino
+++ ESP8266_Webserver_Station_Mode/ESP8266_Webserver_Station_Mode.ino
@@ -1,101 +1,89 @@
+#include <Arduino.h>
+
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
+#include <ESPAsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <FS.h>
 
-/*Put WiFi SSID & Password*/
-const char* ssid = "MHB";   // Enter SSID here
-const char* password = "muhumbania"; // Enter Password here
+AsyncWebServer server(80);
 
-ESP8266WebServer server(80);
+// REPLACE WITH YOUR NETWORK CREDENTIALS
+const char* ssid = "MHB";
+const char* password = "muhumbania";
 
-bool LEDstatus = LOW;
+String indexHtml;// HTML web page
+
+const char* PARAM_INPUT_1 = "input1";
+const char* PARAM_INPUT_2 = "input2";
+const char* PARAM_INPUT_3 = "input3";
+
+void notFound(AsyncWebServerRequest *request) {
+  request->send(404, "text/plain", "Not found");
+}
 
 void setup() {
-  Serial.begin(9600);
-  delay(100);
-  // pinMode(D4, OUTPUT);
-
-  Serial.println("Connecting to ");
-  Serial.println(ssid);
-
-  //connect to your local wi-fi network
+  Serial.begin(115200);
+  WiFi.mode(WIFI_STA);
   WiFi.begin(ssid, password);
-
-  //check NodeMCU is connected to Wi-fi network
-  while (WiFi.status() != WL_CONNECTED) {
-  delay(1000);
-  Serial.print(".");
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("WiFi Failed!");
+    return;
   }
-  Serial.println("");
-  Serial.println("WiFi connected..!");
-  Serial.print("Got IP: ");  
+  Serial.println();
+  Serial.print("IP Address: ");
   Serial.println(WiFi.localIP());
 
-  // server.on("/", handle_OnConnect);
-  // server.on("/ledon", handle_ledon);
-  // server.on("/ledoff", handle_ledoff);
-  // server.onNotFound(handle_NotFound);
+  // Read the contents of the HTML file
+  File file = SPIFFS.open("../index.html", "r");
+  if (!file) {
+      Serial.println("Failed to open index.html file");
+      return;
+  }
 
+  // Read the contents of the file and store it in indexHtml variable
+  while (file.available()) {
+      indexHtml += (char)file.read();
+  }
+
+  file.close();
+
+  // Send web page with input fields to client
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send_P(200, "text/html", indexHtml);
+  });
+
+  // Send a GET request to <ESP_IP>/get?input1=<inputMessage>
+  server.on("/get", HTTP_GET, [] (AsyncWebServerRequest *request) {
+    String inputMessage;
+    String inputParam;
+    // GET input1 value on <ESP_IP>/get?input1=<inputMessage>
+    if (request->hasParam(PARAM_INPUT_1)) {
+      inputMessage = request->getParam(PARAM_INPUT_1)->value();
+      inputParam = PARAM_INPUT_1;
+    }
+    // GET input2 value on <ESP_IP>/get?input2=<inputMessage>
+    else if (request->hasParam(PARAM_INPUT_2)) {
+      inputMessage = request->getParam(PARAM_INPUT_2)->value();
+      inputParam = PARAM_INPUT_2;
+    }
+    // GET input3 value on <ESP_IP>/get?input3=<inputMessage>
+    else if (request->hasParam(PARAM_INPUT_3)) {
+      inputMessage = request->getParam(PARAM_INPUT_3)->value();
+      inputParam = PARAM_INPUT_3;
+    }
+    else {
+      inputMessage = "No message sent";
+      inputParam = "none";
+    }
+    Serial.println(inputMessage);
+    request->send(200, "text/html", "HTTP GET request sent to your ESP on input field (" 
+                                     + inputParam + ") with value: " + inputMessage +
+                                     "<br><a href=\"/\">Return to Home Page</a>");
+  });
+  server.onNotFound(notFound);
   server.begin();
-  Serial.println("HTTP Server Started");
 }
+
 void loop() {
-  server.handleClient();
   
-//   if(LEDstatus)
-//   {
-//     digitalWrite(D4, HIGH);}
-//   else
-//   {
-//     digitalWrite(D4, LOW);}
-}
-
-void handle_OnConnect() {
-  LEDstatus = LOW;
-  Serial.println("LED: OFF");
-  server.send(200, "text/html", updateWebpage(LEDstatus)); 
-}
-
-void handle_ledon() {
-  LEDstatus = HIGH;
-  Serial.println("LED: ON");
-  server.send(200, "text/html", updateWebpage(LEDstatus)); 
-}
-
-void handle_ledoff() {
-  LEDstatus = LOW;
-  Serial.println("LED: OFF");
-  server.send(200, "text/html", updateWebpage(LEDstatus)); 
-}
-
-void handle_NotFound(){
-  server.send(404, "text/plain", "Not found");
-}
-
-String updateWebpage(uint8_t LEDstatus){
-  String ptr = "<!DOCTYPE html> <html>\n";
-  ptr +="<head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0, user-scalable=no\">\n";
-  ptr +="<title>LED Control</title>\n";
-  ptr +="<style>html {font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}\n";
-  ptr +="body{margin-top: 50px;} h1 {color: #444444;margin: 50px auto 30px;} h3 {color: #444444;margin-bottom: 50px;}\n";
-  ptr +=".button {display: block;width: 80px;background-color: #1abc9c;border: none;color: white;padding: 13px 30px;text-decoration: none;font-size: 25px;margin: 0px auto 35px;cursor: pointer;border-radius: 4px;}\n";
-  ptr +=".button-on {background-color: #3498db;}\n";
-  ptr +=".button-on:active {background-color: #3498db;}\n";
-  ptr +=".button-off {background-color: #34495e;}\n";
-  ptr +=".button-off:active {background-color: #2c3e50;}\n";
-  ptr +="p {font-size: 14px;color: #888;margin-bottom: 10px;}\n";
-  ptr +="</style>\n";
-  ptr +="</head>\n";
-  ptr +="<body>\n";
-  ptr +="<h1>ESP8266 Web Server</h1>\n";
-  ptr +="<h3>Using Station(STA) Mode</h3>\n";
-  
-   if(LEDstatus){
-    ptr +="<p>BLUE LED: ON</p><a class=\"button button-off\" href=\"/ledoff\">OFF</a>\n";
-   }else{
-    ptr +="<p>BLUE LED: OFF</p><a class=\"button button-on\" href=\"/ledon\">ON</a>\n";
-   }
-
-  ptr +="</body>\n";
-  ptr +="</html>\n";
-  return ptr;
 }
