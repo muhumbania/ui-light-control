c4ee14fb51afde0a84f2f94c1ea62b06c9226804
muhumbania <franklinmuhumbania@gmail.com>
2024-04-20 15:19:24 +0200

    Fixed the loading images issue and created a public and view directory

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 const http = require('http');
+const express = require('express');
 // import { createServer } from 'http';
 const { SerialPort } =  require('serialport');
+const ejs = require('ejs');
 
 // Import parsers from SerialPort
 // const Readline = require('@serialport/parser-readline');
 // const { Readline } = pkg;
 
 // Read the contents of 'index.html'
-const index = fs.readFileSync('index.html');
+// const index = fs.readFileSync('index.html');
 // const parsers = SerialPort.parsers;
 
 const port = new SerialPort({
@@ -33,13 +35,18 @@ const port = new SerialPort({
 
 // port.pipe(parser);
 
-const app = http.createServer(function(req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(index);
-});
+// const app = http.createServer(function(req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/html'});
+//     res.end(index);
+// });
+
+const app = express();
+app.use(express.static('public'));
+
+const server = http.createServer(app);
 
 const { Server } = require('socket.io');
-const io = new Server(app);
+const io = new Server(server);
 
 
 io.on('connection', function(socket) {
@@ -52,6 +59,10 @@ io.on('connection', function(socket) {
     });
     
 });
+
+app.get('/', function(req, res){
+    res.render('index.ejs');
+})
 
 app.listen(3000, function(){
     console.log('Server listening on port 3000...');
