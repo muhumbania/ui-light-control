ed63d58ddb0508882fad44517a5ca6fc41205012
muhumbania <franklinmuhumbania@gmail.com>
2024-04-20 14:57:41 +0200

    Fixed the library import issues

--- app.js
+++ app.js
@@ -1,16 +1,18 @@
-import fs from 'fs';
+const fs = require('fs');
+const http = require('http');
 // import { createServer } from 'http';
-import { SerialPort } from 'serialport';
+const { SerialPort } =  require('serialport');
 
 // Import parsers from SerialPort
-import pkg from '@serialport/parser-readline';
-const { Readline } = pkg;
+// const Readline = require('@serialport/parser-readline');
+// const { Readline } = pkg;
 
 // Read the contents of 'index.html'
 const index = fs.readFileSync('index.html');
+// const parsers = SerialPort.parsers;
 
 const port = new SerialPort({
-    path: '\Device\Silabser0',
+    path: 'COM3',
     baudRate: 9600,
     dataBits: 8,
     parity: 'none',
@@ -19,22 +21,26 @@ const port = new SerialPort({
 });
 
 // Create a new instance of the Readline parser
-const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+// const parser = port.pipe(new Readline({ delimiter: '\r\n' }));
+
+// const parser = new parsers.Readline({
+//     delimiter: '\r\n'
+// });
 
 
 
 
 
-
-
-port.pipe(parser);
+// port.pipe(parser);
 
 const app = http.createServer(function(req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.end(index);
 });
 
-const io = require('socket.io').listen(app);
+const { Server } = require('socket.io');
+const io = new Server(app);
+
 
 io.on('connection', function(socket) {
     
@@ -47,4 +53,6 @@ io.on('connection', function(socket) {
     
 });
 
-app.listen(3000);
+app.listen(3000, function(){
+    console.log('Server listening on port 3000...');
+});
